aa5e0b6ed83f059ba8165f4136291ed2ba4575ee
meangpu <51825872+meangpu@users.noreply.github.com>
2021-11-04 16:18:15 +0700

    new slider

---
+++ Geopark_Project/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float speed;
+    public bool DoRotate = true;
+    [SerializeField] Slider mainSlider;
+
+    void Update()
+    {
+        if (DoRotate)
+        {
+            transform.Rotate(0, speed*Time.deltaTime, 0);
+        }
+    }
+
+    public void doSwitchRotate()
+    {
+        if (DoRotate)
+        {
+            DoRotate = !DoRotate;
+        }
+        else
+        {
+            DoRotate = !DoRotate;
+        }
+    }
+
+    public void setRotSpeed()
+    {
+        speed = mainSlider.value;
+    }
+}
