2e91eb111255cb7a3b8336fad2176cc50064520c
meangpu <51825872+meangpu@users.noreply.github.com>
2022-03-24 05:27:42 +0700

    audio add finish

---
+++ Geopark_Project/Assets/_Scripts/SoundTranscript.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ButtonAndSoundName
+{
+    public string targetSoundName;
+    public Button button;
+
+    public void changeImage(Sprite _sprite, Color _col)
+    {
+        button.transform.GetChild(0).GetComponent<Image>().sprite = _sprite;
+        button.image.color = _col;
+    }
+
+}
+
+public class SoundTranscript : MonoBehaviour
+{
+    [SerializeField] Sprite imgPlaying;
+    [SerializeField] Sprite imgStop;
+    [SerializeField] ButtonAndSoundName[] allSoundButton;
+    [SerializeField] Color originalCol;
+    [SerializeField] Color playingCol;
+
+
+    AudioSource[] allAudioSources;
+    AudioManager _aud;
+
+    string previousPlay;
+
+
+    private void Start() 
+    {
+
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+
+        _aud = FindObjectOfType<AudioManager>();
+        foreach (ButtonAndSoundName _b in allSoundButton)
+        {
+            _b.button.onClick.AddListener(delegate{playSoundChangeImage(_b.targetSoundName, _b);});
+        }
+
+    }
+
+    public void playSoundChangeImage(string _name, ButtonAndSoundName _nowBut)
+    {
+        // reclick 
+        StopAllAudio();
+        if(previousPlay == _name)
+        {
+            // reset name
+            previousPlay = "";
+            return;
+        }
+        else
+        {
+            _aud.Play(_name);
+            _nowBut.changeImage(imgPlaying, playingCol);
+        }
+
+        previousPlay = _name;
+    }
+
+    void StopAllAudio() 
+    {
+        // เปลี่ยนภาพ เหลือเปลี่ยนการกดได้?
+        foreach (ButtonAndSoundName _b in allSoundButton)
+        {
+            _b.changeImage(imgStop, originalCol);
+        }
+
+        foreach( AudioSource audioS in allAudioSources) 
+        {
+            audioS.Stop();
+        }
+        
+    }
+
+
+
+}
